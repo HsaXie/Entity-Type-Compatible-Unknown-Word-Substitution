6156384b7251dd13bf56ef33c43726fd17486d94
Hsaeno <47032254+Hsaeno@users.noreply.github.com>
2022-04-28 15:38:56 +0800

    Update etc_main.py
    rewrite description

--- etc_main.py
+++ etc_main.py
@@ -15,9 +15,7 @@ parser.add_argument("-lr", "--lr", help="Learning rate", type=float, required=Fa
 parser.add_argument("-drop", "--drop_out", help="Drop out", type=float, required=False, default=0.5)
 parser.add_argument("-margin", "--margin", help="Triplet Loss Margin", type=float, required=False, default=1)
 parser.add_argument("-bs", "--batch_size", help="Train batch size for model", type=int, required=False, default=16)
-parser.add_argument("-k", "--k_num", help="Model will select the best suitable ETC word from k candidates. In paper, "
-                                          "we use 1. And we encourage you to try different numbers to achieve better "
-                                          "result.", type=int, required=False, default=1)
+parser.add_argument("-k", "--k_num", help="Model will select the best suitable ETC word from k candidates. In paper, we use 1. And we encourage you to try different numbers to achieve better result.", type=int, required=False, default=1)
 parser.add_argument("-osp", "--o_sample_prob", help="The probability of sampling O label", type=float, required=False, default=0.02)
 parser.add_argument("-hs", "--hidden_size", help="ETC model hidden size", type=int, required=False, default=512)
 parser.add_argument("-os", "--output_size", help="ETC model output size", type=int, required=False, default=256)
